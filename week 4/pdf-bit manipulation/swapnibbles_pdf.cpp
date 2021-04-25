912719f0430d93b3ffa59b1ba7426952f23cdb9e
sneharaoganta <64457551+sneharaoganta@users.noreply.github.com>
2021-04-25 23:53:48 +0530

    Create swapnibbles_pdf.cpp

---
+++ week 4/pdf-bit manipulation/swapnibbles_pdf.cpp
@@ -0,0 +1,11 @@
+unsigned char swap_nibbles(unsigned char c)
+{
+     unsigned char temp1, temp2;
+
+     temp1 = c & 0x0F;
+     temp2 = c & 0xF0;
+     temp1=temp1 << 4;
+     temp2=temp2 >> 4;
+
+     return(temp2|temp1); //adding the bits
+}
