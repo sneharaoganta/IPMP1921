ac2905351411191fbc8fe44b809bff6e3594d68e
sneharaoganta <64457551+sneharaoganta@users.noreply.github.com>
2021-04-25 23:52:49 +0530

    Create add2nos_pdf.cpp

---
+++ week 4/pdf-bit manipulation/add2nos_pdf.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int Add(int x, int y)
+{
+    while (y != 0)
+    {
+        int carry = x & y;
+        x = x ^ y;
+        y = carry << 1;
+    }
+    return x;
+}
+ 
+int main()
+{
+    cout << Add(15, 32);
+    return 0;
+}
+ 
