171e9e2ef7c31bfeb16885a726cb212d39923226
sneharaoganta <64457551+sneharaoganta@users.noreply.github.com>
2021-05-29 21:48:31 +0530

    Create fibnosub_leetcode.cpp

---
+++ week 6/Divide and Conquer/pdf/fibnosub_leetcode.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/fibonacci-number/
+class Solution {
+public:
+    int fib(int n) {
+       int f[n+2];
+        int i;
+        f[0]=0;
+        f[1]=1;
+        
+        for(i=2;i<=n;i++){
+            f[i]=f[i-1]+f[i-2];
+        }
+        
+        return f[n];
+    }
+};
