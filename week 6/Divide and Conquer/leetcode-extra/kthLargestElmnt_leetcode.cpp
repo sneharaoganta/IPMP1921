d464fe0718009f19e214a4dea1a79f36da141ac2
sneharaoganta <64457551+sneharaoganta@users.noreply.github.com>
2021-05-29 21:47:30 +0530

    Create kthLargestElmnt_leetcode.cpp

---
+++ week 6/Divide and Conquer/leetcode-extra/kthLargestElmnt_leetcode.cpp
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/kth-largest-element-in-an-array/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end(),greater<int>());
+        return nums[k-1];
+    }
+};
