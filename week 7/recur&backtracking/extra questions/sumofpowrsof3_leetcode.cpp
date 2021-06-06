d27ea7392956cf67cdab4df815da4bdb4bcc335c
sneharaoganta <64457551+sneharaoganta@users.noreply.github.com>
2021-06-06 16:36:46 +0530

    Create sumofpowrsof3_leetcode.cpp

---
+++ week 7/recur&backtracking/extra questions/sumofpowrsof3_leetcode.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        
+      while (n > 0) {
+            if (n %3 > 1) {
+                return false;
+            }
+            
+            n = n / 3;
+        }
+        
+        return true;
+        
+    }
+};
